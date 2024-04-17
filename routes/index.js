381291477b31b9ac849e6e583976d45fa3efe427
Tiago Coelho <8220196@estg.ipp.pt>
2024-04-17 13:43:18 +0100

    retirar biblioteca

--- routes/index.js
+++ routes/index.js
@@ -10,10 +10,5 @@ router.get('/login', function(req, res, next) {
   res.render('admin/login');
 });
 
-router.get('/menuAdmin', function(req, res, next) {
-  res.render('admin/menuAdmin');
-});
-
-
 
 module.exports = router;
