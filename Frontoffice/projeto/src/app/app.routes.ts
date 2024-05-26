160d9e9082d2a46d99d92a2ad3b295fda7260544
paul0coelho <paulosmcoelho04@gmail.com>
2024-05-27 00:15:02 +0100

    Criação de componentes básicos e páginas

--- Frontoffice/projeto/src/app/app.routes.ts
+++ Frontoffice/projeto/src/app/app.routes.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { InitialPageComponent } from './initial-page/initial-page.component';
+import { DonatorComponent } from './donator/donator.component';
+import { EntityComponent } from './entity/entity.component';
 
 
 const routes: Routes = [
     {
         path: 'login',
         component: LoginComponent,
+    },
+    {
+        path: 'welcomePage',
+        component: InitialPageComponent,
+    },
+    {
+        path: 'donator',
+        component: DonatorComponent,
+    },
+    {
+        path: 'entity',
+        component: EntityComponent,
     }
 ];
 
---
+++ Frontoffice/projeto/src/app/donator/donator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-donator',
+  standalone: true,
+  imports: [],
+  templateUrl: './donator.component.html',
+  styleUrl: './donator.component.css'
+})
+export class DonatorComponent {
+
+}
