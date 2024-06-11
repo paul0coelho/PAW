f26e78bdacf08bfc8c451192f2700d5a292e09f9
Tiago Coelho <8220196@estg.ipp.pt>
2024-06-11 22:35:37 +0100

    alerta password incorrecta

--- Frontoffice/projeto/src/app/auth/login/login.component.ts
+++ Frontoffice/projeto/src/app/auth/login/login.component.ts
@@ -13,34 +13,39 @@ export class LoginComponent implements OnInit{
 
   email: string;
   password:string;
+  errorMessage: string; 
   
 
   constructor(private router: Router, private authService: AuthService) { 
     this.email="";
     this.password="";
-    
-    
+    this.errorMessage = "";
   }
 
   ngOnInit(): void {
   }
 
- login(): void {
-  this.authService.login(this.email, this.password).subscribe((response: any) => {
-    if (response && response.token) {
-      localStorage.setItem('accessToken', JSON.stringify(response));
-      if (response.userType === 'donator') {
-        this.router.navigate(['/donator']);
-      } else if (response.userType === 'entity') {
-        this.router.navigate(['/entity']); 
-      } else {
-        this.router.navigate(['/error']); 
+  login(): void {
+    this.authService.login(this.email, this.password).subscribe(
+      (response: any) => {
+        if (response && response.token) {
+          localStorage.setItem('accessToken', JSON.stringify(response));
+          if (response.userType === 'donator') {
+            this.router.navigate(['/donator']);
+          } else if (response.userType === 'entity') {
+            this.router.navigate(['/entity']); 
+          } else {
+            this.router.navigate(['/error']); 
+          }
+        } else {
+          this.errorMessage = 'Erro no login!';
+        }
+      },
+      (error) => {
+        this.errorMessage = 'Email ou password incorretos!'; 
       }
-    } else {
-      alert('Erro no login!');
-    }
-  });
-}
+    );
+  }
  
 
   register(): void {
